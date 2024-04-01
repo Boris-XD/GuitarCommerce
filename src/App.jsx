46594f39dbb2f88a7a16e02be80157614b8a0c91
Boris Sea <seaborisrc@gmail.com>
2024-03-31 22:09:48 -0400

    guitar - Add state

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,44 @@
+import { useState } from "react"
 import Footer from "./components/Footer"
 import Guitar from "./components/Guitar"
 import Header from "./components/Header"
+import { db } from "./db/db"
 
 const App = () => {
+
+  const [data] = useState(db);
+  const [cart, setCart] = useState([]);
+
+  function addToCart(guitar){
+    const itemExist = cart.findIndex(item => item.id == guitar.id);
+
+    if (itemExist == -1){
+      setCart([...cart, {...guitar, quantity: 1}])
+    }else{
+      const updateCart = [...cart];
+      updateCart[itemExist].quantity++;
+      setCart(updateCart);
+    }
+
+    console.log(cart)
+  }
+
   return (
     <>
-
-    <Header />      
-
+    <Header />  
     <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-            <Guitar />
+          {
+            data.map(guitar =>
+              <Guitar
+                key={guitar.id}
+                auth={true}
+                guitar={guitar}
+                addToCart={addToCart}
+                />)
+          }
         </div>
     </main>
     <Footer />
